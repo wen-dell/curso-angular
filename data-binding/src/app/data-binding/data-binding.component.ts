013c733bbb2133e72537d02271ae5a9ff584e1c3
wendellmccaulay@hotmail.com <wendellmccaulay@hotmail.com>
2018-01-30 16:36:55 -0300

    Class, Style, Event e Two-Way Data Binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -9,10 +9,51 @@ export class DataBindingComponent implements OnInit {
 
   url: string = 'http://loiane.com';
   urlImagem: string = 'https://octodex.github.com/images/Professortocat_v2.png';
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
+  ativada: boolean = false;
+  nome: string = 'abc';
+
+  pessoa: any = {
+    nome: 'Wendell',
+    idade: 20
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  botaoClicado() {
+    alert("Botão foi clicado!");
+  }
+
+  onKeyUp(evento : KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  exibirMensagem() {
+    alert("Você é demais! ;)");
+  }
+
+  verificarEntrada(evento: KeyboardEvent) {
+    if (this.ativada) {
+      this.ativada = false;
+    } else {
+      if (evento.key == 'CapsLock') {
+        this.ativada = true;
+        alert("Tecla Capslock está ativada!");
+      }
+    }
   }
 
 }
