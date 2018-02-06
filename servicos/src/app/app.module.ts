b6d2dd6e9d300f4a60b52790276429ec594d71a6
wendellmccaulay@hotmail.com <wendellmccaulay@hotmail.com>
2018-02-06 19:56:27 -0300

    Injetando um serviço em outro

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CursosComponent } from './cursos/cursos.component';
-import { CursosService } from 'app/cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { CursosModule } from './cursos/cursos.module';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,7 @@ import { CursosModule } from './cursos/cursos.module';
     CriarCursoModule,
     CursosModule
   ],
-  //providers: [CursosService],
+  providers: [LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 
+import { LogService } from './../shared/log.service';
+
 @Injectable()
 export class CursosService {
 
@@ -7,15 +9,17 @@ export class CursosService {
     emitirCursoCriado = new EventEmitter<string>();
     static criouNovoCurso = new EventEmitter<string>();
 
-    constructor() {
+    constructor(private logService: LogService) {
         console.log('CursosService');
     }
 
     getCursos(): string[] {
+        this.logService.consoleLog('Obtendo lista de cursos')
         return this.cursos;
     }
 
     addCurso(curso: string) {
+        this.logService.consoleLog('Criando um novo curso ' + curso);
         this.cursos.push(curso);
         this.emitirCursoCriado.emit(curso);
         CursosService.criouNovoCurso.emit(curso);
