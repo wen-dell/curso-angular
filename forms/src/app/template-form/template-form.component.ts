ed048f1cfbee908013abc7d41b24ffc46f63919b
wendellmccaulay@hotmail.com <wendellmccaulay@hotmail.com>
2018-02-13 11:39:56 -0300

    setValue e patchValue

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -36,20 +36,61 @@ export class TemplateFormComponent implements OnInit {
     }
   }
 
-  consultaCEP(cep) {
+  consultaCEP(cep, form) {
     cep = cep.replace(/\D/g, '');
     if (cep != "") {
       let validaCep = /^[0-9]{8}$/;
 
       if (validaCep.test(cep)) {
+
+        this.resetaDadosForm(form);
+
         this.http.get(`//viacep.com.br/ws/${cep}/json`)
           .map(cep => cep.json())
           .subscribe(cep => {
-            console.log(cep);
+            this.populaDadosForm(cep, form);
           });
       }
 
     }
+  }
+
+  populaDadosForm(dados, formulario) {
+    /*formulario.setValue({
+      nome: formulario.value.nome,
+      email: formulario.value.email,
+      endereco: {
+        cep: dados.cep,
+        numero: '',
+        complemento: dados.complemento,
+        rua: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });*/
+
+    formulario.form.patchValue({
+      endereco: {
+        complemento: dados.complemento,
+        rua: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+  }
+
+  resetaDadosForm(formulario) {
+    formulario.form.patchValue({
+      endereco: {
+        complemento: null,
+        rua: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });
   }
 
 }
