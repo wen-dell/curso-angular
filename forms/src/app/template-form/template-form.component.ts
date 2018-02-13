72a00230a4e7be7f6dc8b5880e916c7dc3c340ca
wendellmccaulay@hotmail.com <wendellmccaulay@hotmail.com>
2018-02-12 21:42:30 -0300

    Aplicando validações com CSS

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: 'Wendell',
-    email: 'wendell@email.com'
+    nome: null,
+    email: null
   };
 
   constructor() { }
